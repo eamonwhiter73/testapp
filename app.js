5c413bc880c501d0b377d0b33a9098be5e1e2a90
eamonwhiter73 <eamon.white7@gmail.com>
2014-05-21 18:50:59 -0400

    help

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+/*var pf = require('policyfile').createServer();
+
+pf.listen(10843, function(){
+  console.log(':3 yay')
+});*/
+
+server.listen(8000);
+
+app.use(express.static(__dirname + '/'));
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/Startup.html');
+});
+
+io.configure(function() {
+  io.set('transports', ['websocket','flashsocket']);
+  io.set('flash policy port', 10843);
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('message', function (data) {
+    console.log(data);
+  });
+});
